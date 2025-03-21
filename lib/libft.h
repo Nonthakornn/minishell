802891e6def1b6a0b0956cd3467b8345404e2a01
Naeramit Ridtiya <nridtiya@z2t7c1.42bangkok.com>
2025-03-21 12:05:01 +0700

    edut libft.h

--- lib/libft.h
+++ lib/libft.h
@@ -16,21 +16,18 @@ int		count_str_arr(char *arr[]);
 void	free_str_arr(char **arr);
 void	edit_str_arr(char **str_arr, int edited_index, char *new_str);
 void	print_str_arr(char **str_arr);
-
 char	*str_join(char *s1, char *s2);
-
-char	**str_split(char *str, char *sep);
-
-int		find_index(char *str, char c);
-int		ft_strcmp(char *s1, char *s2, char terminal);
 char	*slice(char *src, size_t start, size_t end);
-bool	is_space(int c);
-char	*new_str(int len);
+char	**str_split(char *str, char *sep);
 void	print_str(int fd, char *str);
-void	ft_strcpy(char *dst, char *src);
 void	puterror(char *str);
+char	*new_str(int len);
+void	ft_strcpy(char *dst, char *src);
+int		ft_strcmp(char *s1, char *s2, char terminal);
 size_t	ft_strlen(char *str);
-int		startwith(char *str, char *word);
+int		find_index(char *str, char c);
 int		is_equal(char *s1, char *s2);
 int		is_include(char *str, char c);
+bool	is_space(int c);
+int		startwith(char *str, char *word);
 #endif
